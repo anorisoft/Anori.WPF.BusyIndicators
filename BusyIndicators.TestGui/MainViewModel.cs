d81444c71eb190c741341e2728286b259591a6d9
Martin <Martin@eglinet.ch>
2020-03-08 21:10:08 +0100

    Add project files.

---
+++ BusyIndicators.TestGui/MainViewModel.cs
@@ -0,0 +1,123 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="MainViewModel.cs" company="Anori Soft">
+// Copyright (c) Anori Soft. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BusyIndicatorsGui
+{
+    using System.Diagnostics;
+    using System.Runtime.CompilerServices;
+    using System.Windows.Input;
+
+    using JetBrains.Annotations;
+
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private bool isBusy;
+
+        public MainViewModel()
+        {
+            this.BusyCommand = new RelayCommand<object>(this.OnBusy);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand BusyCommand { get; }
+
+        public bool IsBusy
+        {
+            get => this.isBusy;
+            set
+            {
+                if (value == this.isBusy)
+                {
+                    return;
+                }
+
+                this.isBusy = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        private void OnBusy(object obj)
+        {
+            var b = (bool)obj;
+            this.IsBusy = b;
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class RelayCommand<T> : ICommand
+    {
+        #region Fields
+
+        private readonly Action<T> _execute;
+
+        private readonly Predicate<T> _canExecute;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public RelayCommand(Action<T> execute)
+            : this(execute, null)
+        {
+        }
+
+        /// <summary>
+        ///     Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+
+            this._execute = execute;
+            this._canExecute = canExecute;
+        }
+
+        #endregion Constructors
+
+        #region ICommand Members
+
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameter)
+        {
+            return this._canExecute == null ? true : this._canExecute((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            this._execute((T)parameter);
+        }
+
+        #endregion ICommand Members
+    }
+}
---
+++ BusyInticators/DispatcherExtensions.cs
@@ -0,0 +1,194 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="DispatcherExtensions.cs" company="Anori Soft">
+// Copyright (c) Anori Soft. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace BusyIndicators
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Threading;
+
+    using JetBrains.Annotations;
+
+    /// <summary>
+    ///     Dispatcher Extensions
+    /// </summary>
+    public static class DispatcherExtensions
+    {
+        /// <summary>
+        ///     Dispatches the specified action.
+        /// </summary>
+        /// <param name="dependencyObject">The dependency object.</param>
+        /// <param name="action">The action.</param>
+        /// <exception cref="ArgumentNullException">
+        ///     dependencyObject
+        ///     or
+        ///     action
+        ///     or
+        ///     dispatcher
+        /// </exception>
+        public static void Dispatch([NotNull] this DependencyObject dependencyObject, [NotNull] Action action)
+        {
+            if (dependencyObject == null)
+            {
+                throw new ArgumentNullException(nameof(dependencyObject));
+            }
+
+            if (action == null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            var dispatcher = dependencyObject.Dispatcher;
+            if (dispatcher == null)
+            {
+                throw new ArgumentNullException(nameof(dispatcher));
+            }
+
+            if (dispatcher.CheckAccess())
+            {
+                action();
+            }
+            else
+            {
+                dispatcher.BeginInvoke(action);
+            }
+        }
+
+        /// <summary>
+        ///     Dispatches the specified action.
+        /// </summary>
+        /// <typeparam name="TDependencyObject">The type of the dependency object.</typeparam>
+        /// <param name="dependencyObject">The dependency object.</param>
+        /// <param name="action">The action.</param>
+        /// <exception cref="ArgumentNullException">
+        ///     dependencyObject
+        ///     or
+        ///     action
+        ///     or
+        ///     dispatcher
+        /// </exception>
+        public static void Dispatch<TDependencyObject>(
+            [NotNull] this TDependencyObject dependencyObject,
+            [NotNull] Action<TDependencyObject> action)
+            where TDependencyObject : DependencyObject
+        {
+            if (dependencyObject == null)
+            {
+                throw new ArgumentNullException(nameof(dependencyObject));
+            }
+
+            if (action == null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            var dispatcher = dependencyObject.Dispatcher;
+            if (dispatcher == null)
+            {
+                throw new ArgumentNullException(nameof(dispatcher));
+            }
+
+            if (dispatcher.CheckAccess())
+            {
+                action(dependencyObject);
+            }
+            else
+            {
+                dispatcher.BeginInvoke(new Action(() => action(dependencyObject)));
+            }
+        }
+
+        /// <summary>
+        ///     Dispatches the specified dispatcher priority.
+        /// </summary>
+        /// <typeparam name="TDependencyObject">The type of the dependency object.</typeparam>
+        /// <param name="dependencyObject">The dependency object.</param>
+        /// <param name="dispatcherPriority">The dispatcher priority.</param>
+        /// <param name="action">The action.</param>
+        /// <exception cref="ArgumentNullException">
+        ///     dependencyObject
+        ///     or
+        ///     action
+        ///     or
+        ///     dispatcher
+        /// </exception>
+        public static void Dispatch<TDependencyObject>(
+            [NotNull] this TDependencyObject dependencyObject,
+            DispatcherPriority dispatcherPriority,
+            [NotNull] Action<TDependencyObject> action)
+            where TDependencyObject : DependencyObject
+        {
+            if (dependencyObject == null)
+            {
+                throw new ArgumentNullException(nameof(dependencyObject));
+            }
+
+            if (action == null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            var dispatcher = dependencyObject.Dispatcher;
+            if (dispatcher == null)
+            {
+                throw new ArgumentNullException(nameof(dispatcher));
+            }
+
+            if (dispatcher.CheckAccess())
+            {
+                action(dependencyObject);
+            }
+            else
+            {
+                dispatcher.BeginInvoke(dispatcherPriority, new Action(() => action(dependencyObject)));
+            }
+        }
+
+        /// <summary>
+        ///     Dispatches the specified dispatcher priority.
+        /// </summary>
+        /// <param name="dependencyObject">The dependency object.</param>
+        /// <param name="dispatcherPriority">The dispatcher priority.</param>
+        /// <param name="action">The action.</param>
+        /// <exception cref="ArgumentNullException">
+        ///     dependencyObject
+        ///     or
+        ///     action
+        ///     or
+        ///     dispatcher
+        /// </exception>
+        public static void Dispatch(
+            [NotNull] this DependencyObject dependencyObject,
+            DispatcherPriority dispatcherPriority,
+            [NotNull] Action action)
+        {
+            if (dependencyObject == null)
+            {
+                throw new ArgumentNullException(nameof(dependencyObject));
+            }
+
+            if (action == null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            var dispatcher = dependencyObject.Dispatcher;
+            if (dispatcher == null)
+            {
+                throw new ArgumentNullException(nameof(dispatcher));
+            }
+
+            if (dispatcher.CheckAccess())
+            {
+                action();
+            }
+            else
+            {
+                dispatcher.BeginInvoke(dispatcherPriority, action);
+            }
+        }
+    }
+}
